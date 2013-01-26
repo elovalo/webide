38fe3f40aa0e6387f67e278b3b417dd83312fdf3
Juho Vepsalainen <bebraw@gmail.com>
2013-01-26 21:31:51 +0200

    Replace ace with codemirror
    Seems to work better with requirejs.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 require.config({
     paths: {
         jquery: '../components/jquery/jquery',
-        threejs: '../ext/three'
+        threejs: '../ext/three',
+        codemirror: '../components/codemirror/lib/codemirror'
     },
     urlArgs: 'buster=' + (new Date()).getTime() // for dev only! use rev otherwise
 });
 
-require(['jquery', 'threejs'], function($) {
+require(['jquery', 'codemirror', 'threejs'], function($) {
     $(function() {
         // TODO: draw some leds now
         // TODO: draw ide
