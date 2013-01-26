6d3f051aa85803c1008fbff3d7c8965906b667b2
Juho Vepsalainen <bebraw@gmail.com>
2013-01-26 21:05:06 +0200

    Set up RequireJS

---
+++ src/main.js
@@ -0,0 +1,13 @@
+require.config({
+    paths: {
+        jquery: '../components/jquery/jquery'
+    }
+});
+
+require(['jquery'], function($) {
+    $(function() {
+        // TODO: draw some leds now
+        // TODO: draw ide
+        // TODO: eval
+    });
+});
