168b887ad051f8f7142b6a0f50578c909005e37e
Juho Vepsalainen <bebraw@gmail.com>
2013-01-27 11:29:12 +0200

    Change editor to use a realistic code example

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,7 +1,7 @@
 define(['codemirror'], function() {
     function initEditor($e) {
         return CodeMirror($e.get(0), {
-            value: "function a() {\n    return math.clamp(42, 0, 30);\n}\na();",
+            value: "function effect() {\n    cube().off();\n}",
             mode: 'javascript',
             indentUnit: 4,
             lineWrapping: true,
