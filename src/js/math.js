37fd8f3f17c4abc2b859d3fe70580567558bca68
Juho Vepsalainen <bebraw@gmail.com>
2013-01-27 10:54:48 +0200

    Define a couple of basic math utilities

--- src/js/math.js
+++ src/js/math.js
@@ -1,2 +1,10 @@
-// randint
-// clamp
+define({
+    randint: function(min, max) {
+        if(max < min) return;
+
+        return Math.ceil(Math.random() * (max + 1 - min)) - 1 + min;
+    },
+    clamp: function(a, min, max) {
+        return Math.max(min, Math.min(max, a));
+    }
+});
