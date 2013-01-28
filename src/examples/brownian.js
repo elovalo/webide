ce19c08dff88d694809fefc57fd849a4a81a09a4
Juho Vepsalainen <bebraw@gmail.com>
2013-01-28 23:06:37 +0200

    Move map as a function
    The idea is that it will work for both arrays and objects. This way we do not
    need to add non-standard hacks to objects themselves and the result is more
    functional.

--- src/examples/brownian.js
+++ src/examples/brownian.js
@@ -1,13 +1,13 @@
 function init(cube) {
     return {
-        xyz: cube.xyz.map(partial(math.rdiv, 2))
+        xyz: map(partial(math.rdiv, 2), cube.xyz)
     };
 }
 
 function effect(cube, vars) {
     cube(vars.xyz).on();
 
-    vars.xyz = vars.xyz.map(function(v) {
+    vars.xyz = map(function(v) {
         return math.clamp(v + math.randint(-2, 2), 0, cube.x - 1);
-    });
+    }, vars.xyz);
 }
