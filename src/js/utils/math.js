0bca4261465a71035a1c35c5b70b98f111595f67
Juho Vepsalainen <bebraw@gmail.com>
2013-01-29 12:39:12 +0200

    Annotate math module

--- src/js/utils/math.js
+++ src/js/utils/math.js
@@ -1,10 +1,21 @@
-define({
-    randint: function(min, max) {
+define(function(require) {
+    var annotate = require('./annotate');
+    var is = require('./is');
+
+    function randint(min, max) {
         if(max < min) return;
 
         return Math.ceil(Math.random() * (max + 1 - min)) - 1 + min;
-    },
-    clamp: function(a, min, max) {
+    }
+
+    function clamp(a, min, max) {
         return Math.max(min, Math.min(max, a));
     }
+
+    return {
+        randint: annotate('randint', 'Chooses a number between given bounds, bounds included').
+            on(is.number, is.number, randint),
+        clamp: annotate('clamp', 'Clamps given number between given bounds').
+            on(is.number, is.number, is.number, clamp)
+    };
 });
