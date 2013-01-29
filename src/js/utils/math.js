92ba2daf4e1081c93df3494e94c8bcaa619138a4
Juho Vepsalainen <bebraw@gmail.com>
2013-01-29 12:42:43 +0200

    Implement rdiv

--- src/js/utils/math.js
+++ src/js/utils/math.js
@@ -12,10 +12,16 @@ define(function(require) {
         return Math.max(min, Math.min(max, a));
     }
 
+    function rdiv(y, x) {
+        return x / y;
+    }
+
     return {
         randint: annotate('randint', 'Chooses a number between given bounds, bounds included').
             on(is.number, is.number, randint),
         clamp: annotate('clamp', 'Clamps given number between given bounds').
-            on(is.number, is.number, is.number, clamp)
+            on(is.number, is.number, is.number, clamp),
+        rdiv: annotate('rdiv', 'Right associative div operator').
+            on(is.number, is.number, rdiv)
     };
 });
