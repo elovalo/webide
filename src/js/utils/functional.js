5859fb36d0be7c96475a83433ea8e3cda5812915
Juho Vepsalainen <bebraw@gmail.com>
2013-01-29 09:44:42 +0200

    Add various utilities
    It's probably neatest to integrate funkit to the project later. This is good
    enough for now, though.

--- src/js/utils/functional.js
+++ src/js/utils/functional.js
@@ -1,5 +1,9 @@
-define('functional', {
-    partial: function(fn) {
+define('functional', function(require) {
+    var annotate = require('./annotate');
+    var is = require('./is');
+    var obj = require('./object');
+
+    function partial(fn) {
         var slice = Array.prototype.slice;
         var args = slice.apply(arguments, [1]);
         var ret = function() {
@@ -14,4 +18,69 @@ define('functional', {
 
         return ret;
     }
+
+    var map = annotate('map', 'Maps using given callback').
+        on(is.fn, is.array, mapArray).
+        on(is.fn, is.object, mapObject);
+
+    function mapArray(cb, o) {
+        var ret = [];
+
+        each(function(v, i) {
+            ret.push(cb(v, i));
+        }, o);
+
+        return ret;
+    }
+
+    function mapObject(cb, o) {
+        return ziptoo(zip(obj.keys(o), map(cb, obj.values(o))));
+    }
+
+    var zip = annotate('zip', 'Converts given arrays into a zip.').
+        on(is.array, is.array, function(a, b) {
+            var ret = [];
+
+            for(var i = 0, len = Math.min(a.length, b.length); i < len; i++) {
+                ret.push([a[i], b[i]]);
+            }
+
+            return ret;
+        });
+
+    var ziptoo = annotate('ziptoo', 'Converts given zip to object.').
+        on(is.array, function(a) {
+            var ret = {};
+
+            each(function(v) {
+                ret[v[0]] = v[1];
+            }, a);
+
+            return ret;
+        });
+
+    var each = annotate('each', 'Executes given `callback` on each item').
+        on(is.fn, is.array, function(cb, o) {
+            for(var i = 0, len = o.length; i < len; i++) {
+                var v = o[i];
+
+                cb(v, i);
+            }
+        }).
+        on(is.fn, is.object, function(cb, o) {
+            var i = 0;
+
+            for(var k in o) {
+                cb(k, o[k], i);
+                i++;
+            }
+        });
+
+    return {
+        partial: partial,
+        map: map,
+        ziptoo: ziptoo,
+        zip: zip,
+        each: each
+    };
 });
--- src/js/utils/object.js
+++ src/js/utils/object.js
@@ -1,1 +1,23 @@
-// clone
+define(function(require) {
+    var annotate = require('./annotate');
+    var is = require('./is');
+
+    function keys(o) {
+        return Object.keys(o);
+    }
+
+    function values(o) {
+        var ret = [];
+
+        for(var k in o) ret.push(o[k]);
+
+        return ret;
+    }
+
+    return {
+        keys: annotate('keys', 'Returns keys of the given object.').
+            on(is.object, keys),
+        values: annotate('values', 'Returns values of the given object.').
+            on(is.object, values)
+    };
+});
