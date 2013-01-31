f545745ced5306c9681825ec93323cce65332f49
Juho Vepsalainen <bebraw@gmail.com>
2013-01-31 16:24:56 +0200

    Simplify matrix example

--- src/examples/matrix.js
+++ src/examples/matrix.js
@@ -8,7 +8,7 @@ function effect(cube, vars) {
     vars.dots = vars.dots.map(function(dot) {
         cube({x: range(cube.x), y: range(cube.y), z: range(dot.z, dot.z + 3)}).on();
 
-        dot.z = dot.z + 1 >= cube.z? 0: dot.z + 1;
+        dot.z = (dot.z + 1) % cube.z;
 
         return dot;
     });
