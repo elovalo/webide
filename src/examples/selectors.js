6250b0864193caa1c2f534fcbd67360e5239e92c
Juho Vepsalainen <bebraw@gmail.com>
2013-01-31 19:19:36 +0200

    Add new selectors

--- src/examples/selectors.js
+++ src/examples/selectors.js
@@ -1,8 +1,19 @@
 function init(cube) {
-    // examples of led selector syntax
-    cube().on(); // whole cube
-    cube({x: range(cube.x - 3)}).on(); // all x from 0 to cube.x - 3 - 1 -> whole
-    cube({x: 3, y: 7}).on(); // all x = 3, y = 7 (z is free)
-    cube({x: 2, y: 4, z: 1}).on(); // specific led
-    cube([2, 4, 1]).on(); // specific led, accepts array too
+    // whole cube
+    cube().on();
+
+    // all x from 0 to cube.x - 3 - 1 -> whole
+    cube({x: range(cube.x - 3)}).on();
+
+    // all x = 3, y = 7 (z is free)
+    cube({x: 3, y: 7}).on();
+
+    // specific led
+    cube({x: 2, y: 4, z: 1}).on();
+    cube([2, 4, 1]).on();
+    cube(2, 4, 1).on();
+
+    // multiple specific leds
+    cube([{x: 1, y: 2, z: 3}, {x: 4, y: 5, z: 6}]).on();
+    cube([[1, 2, 3], [4, 5, 6]]);
 }
