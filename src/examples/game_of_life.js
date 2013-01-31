0e23d7eb31c74e017fc851b90b9bc73c8b9dbe2f
Juho Vepsalainen <bebraw@gmail.com>
2013-01-31 15:03:03 +0200

    Add game of life example

---
+++ src/examples/game_of_life.js
@@ -0,0 +1,17 @@
+function init() {
+    shape.heart(wf.max);
+}
+
+function effect(cube, vars) {
+    var isAlive = false;
+
+    cube().forEach(function(led) {
+        var neighbours = led.neighbours().length;
+
+        if(neighbours >= 6 && neighbours <= 15 || math.randint(0, 10) > 8) {
+            isAlive = true;
+            cube(led).on();
+        }
+        else cube(led).off();
+    });
+}
