96f3fac9114eb4427271215b05dbe5018d9b70e1
Juho Vepsalainen <bebraw@gmail.com>
2013-01-31 15:25:53 +0200

    Replace Math with math
    It is better to include native math functions into our own module for the sake
    of consistency.

--- src/examples/countdown.js
+++ src/examples/countdown.js
@@ -8,5 +8,5 @@ function effect(cube, vars) {
     text.scroll(vars.cur, 9, axis.yz);
     text.scroll(vars.cur, 9 + cube.y, axis.xy);
 
-    vars.cur = Math.max(0, vars.cur - 1);
+    vars.cur = math.max(0, vars.cur - 1);
 }
--- src/examples/ilkka.js
+++ src/examples/ilkka.js
@@ -2,12 +2,12 @@ function effect(cube) {
     var time = ticks / 100;
 
     cube({x: range(cube.x), y: range(cube.y)}).forEach(function(led) {
-        var tc = Math.cos(time);
-        var ts = Math.sin(time);
+        var tc = math.cos(time);
+        var ts = math.sin(time);
         var x = led.x * tc - y * ts;
         var y = led.x * ts + y * tc;
 
-        led.z = (1 + Math.sin(0.8 * (x + y + time)));
+        led.z = (1 + math.sin(0.8 * (x + y + time)));
         cube(led).on();
     });
 }
