7570a420cbb813a301026e6a3639cbdde63af68c
Juho Vepsalainen <bebraw@gmail.com>
2013-01-31 16:24:26 +0200

    Add starfield example

---
+++ src/examples/starfield.js
@@ -0,0 +1,15 @@
+function init(cube) {
+    return {
+        stars: range(10).map(partial(randint, 0, cube.xyz))
+    };
+}
+
+function effect(cube, vars) {
+    vars.stars = vars.stars.map(function(star) {
+        cube(star).on();
+
+        star.y = (star.y + 1) % cube.y;
+
+        return star;
+    });
+}
