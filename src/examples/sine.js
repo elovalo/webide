d475d33324916eace3cb9ea69d5b017261ed561b
Juho Vepsalainen <bebraw@gmail.com>
2013-02-03 21:02:09 +0200

    Make sine example code valid

--- src/examples/sine.js
+++ src/examples/sine.js
@@ -1,6 +1,6 @@
 function effect(cube) {
     cube({z: 0}).map(function(led) {
-        led.z = 0.25 * (2 + math.sin(led.x / 2 + ticks / 25) + sin(y / 2 + ticks / 50));
+        led.z = 0.25 * (2 + math.sin(led.x / 2 + ticks / 25) + math.sin(led.y / 2 + ticks / 50));
 
         return led;
     }).on();
