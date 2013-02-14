89666b9f54ac37e5961deb13c69c85e22881f270
Juho Vepsalainen <bebraw@gmail.com>
2013-02-14 22:31:50 +0200

    Sketch out basic led selection API
    Still need to implement certain bits such as plane selection and groups.

--- public/examples/selectors/basic.js
+++ public/examples/selectors/basic.js
@@ -8,6 +8,9 @@ function init(cube) {
     // all x = 3, y = 7 (z is free)
     cube({x: 3, y: 7}).on();
 
+    // all x = 3, y = [2, 7] (z is free)
+    cube({x: 3, y: [2, 7]}).on();
+
     // specific led
     cube({x: 2, y: 4, z: 1}).on();
     cube([2, 4, 1]).on();
--- public/js/utils/cube.js
+++ public/js/utils/cube.js
@@ -22,24 +22,42 @@ function evaluateEffect(effect, vars, dims) {
 }
 
 function cube(ops, dims) {
-    var ret = function(selector) {
+    var ret = function(selector, b, c) {
+        var functional = funkit.functional;
         var o = {};
         var coords = [];
 
-        if(selector) {
-            // TODO: expand
-            if(is.array(selector)) {
-                coords = selector;
-            }
+        if(is.number(selector)) check([selector, b, c]);
+        else if(is.array(selector)) check(selector);
+        else if(is.object(selector)) check([selector.x, selector.y, selector.z]);
+        else selectAll();
+
+        function check(xyz) {
+            var freeAxes = functional.map(functional.not(is.set), xyz);
+            var freeLen = functional.filter(funkit.id, freeAxes).length;
+
+            if(freeLen > 1) selectAll();
+            else if(freeLen === 1) selectPlanes(xyz);
+            else select(xyz);
         }
-        else {
+
+        function selectAll() {
             for(var x = 0, xLen = dims.x; x < xLen; x++) {
                 for(var y = 0, yLen = dims.y; y < yLen; y++) {
                     for(var z = 0, zLen = dims.z; z < zLen; z++) {
-                        coords.push({x: x, y: y, z: z});
+                        select([x, y, z]);
                     }
                 }
             }
+        }
+
+        function selectPlanes(xyz) {
+            // TODO
+            console.log('select planes', xyz, 'now');
+        }
+
+        function select(xyz) {
+            coords.push({x: xyz[0], y: xyz[1], z: xyz[2]});
         }
 
         o.on = function() {
