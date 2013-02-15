9fc938adad1949eca616b6301a8ea4df9ec02013
Juho Vepsalainen <bebraw@gmail.com>
2013-02-15 09:37:47 +0200

    Remove multiple led selector from spec
    This needs some more thinking. Perhaps a chaining syntax would be more
    appropriate instead.

--- public/examples/selectors/basic.js
+++ public/examples/selectors/basic.js
@@ -2,7 +2,7 @@ function init(cube) {
     var demo = 0;
 
     cube().off();
-    [wholeCube, twoFreedoms, oneFreedom, oneFreedomTwoYs, specificLed, multipleLeds][demo](cube);
+    [wholeCube, twoFreedoms, oneFreedom, oneFreedomTwoYs, specificLed][demo](cube);
 }
 
 function wholeCube(cube) {
@@ -28,9 +28,4 @@ function specificLed(cube) {
     cube({x: 2, y: 4, z: 1}).on();
     cube([3, 5, 2]).on();
     cube(4, 6, 3).on();
-}
-
-function multipleLeds(cube) {
-    cube([{x: 1, y: 2, z: 3}, {x: 4, y: 5, z: 6}]).on();
-    cube([[3, 3, 3], [6, 6, 6]]).on();
 }
