e5c2e7d1feb465495e3f706f5dc5f2ba0f65d587
Juho Vepsalainen <bebraw@gmail.com>
2013-02-17 15:29:54 +0200

    Make default code more sensible (on instead of off)
    Probably should use a proper template here, though.

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,7 +1,7 @@
 define(['codemirror', 'matchbrackets', 'continuecomment', 'javascripthint', 'javascriptmode'], function() {
     function initEditor($e) {
         return CodeMirror($e.get(0), {
-            value: "function effect(cube) {\n    cube().off();\n}",
+            value: "function effect(cube) {\n    cube().on();\n}",
             mode: 'javascript',
             indentUnit: 4,
             lineWrapping: true,
