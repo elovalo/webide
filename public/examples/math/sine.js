edb77565aa93994468702ba6b3668af08c5f4963
Juho Vepsalainen <bebraw@gmail.com>
2013-02-17 15:51:09 +0200

    Slow down sine effect

--- public/examples/math/sine.js
+++ public/examples/math/sine.js
@@ -1,7 +1,7 @@
 function effect(cube) {
     cube().off();
     cube({z: 0}).map(function(led) {
-        led.z = 0.25 * (2 + math.sin(led.x / 2 + ticks / 25) + math.sin(led.y / 2 + ticks / 50)) * cube.z;
+        led.z = 0.25 * (2 + math.sin(led.x / 2 + ticks / 300) + math.sin(led.y / 2 + ticks / 400)) * cube.z;
 
         return led;
     }).on();
