c0542b522db25ebcfe1f3b1c80c36a76286046dd
Juho Vepsalainen <bebraw@gmail.com>
2013-02-18 15:22:59 +0200

    Make example code valid

--- public/examples/animation/wave.js
+++ public/examples/animation/wave.js
@@ -8,5 +8,5 @@ function init() {
 function effect(cube, vars) {
     vars.avg = (0.9 * vars.avg) + (0.1 * vars.inputs.distance1);
 
-    cube({z: 0}).on(0.25 * (2 * math.sin(x * 50 * ticks / 15 + vars.avg / 10)));
+    cube({z: 0}).on(0.25 * (2 * math.sin(cube.x * 50 * ticks / 15 + vars.avg / 10)));
 }
