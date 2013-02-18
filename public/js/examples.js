effeae405dd37c5ffcc09dc354523f249639717f
Juho Vepsalainen <bebraw@gmail.com>
2013-02-18 15:35:01 +0200

    Update examples entries

--- public/js/examples.js
+++ public/js/examples.js
@@ -1,6 +1,6 @@
 define({
-    'animation': ['finite_worm','blinker','game_of_life','gradient','layers','matrix','rain','starfield','tornado','tube','wave','wireframe','worm'],
-    'inputs': ['test_inputs'],
+    'animation': ['blinker','finite_worm','game_of_life','gradient','layers','matrix','rain','starfield','tornado','tube','wireframe','worm'],
+    'inputs': ['test_inputs','wave'],
     'math': ['ilkka','sine'],
     'particles': ['basic','brownian'],
     'selectors': ['all_off','all_on','basic','constant','corners','filter','stairs_x','stairs_y'],
