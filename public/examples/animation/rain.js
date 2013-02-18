a06649d0ba777d50e349ed8a1aa3fd6711eedb7f
Juho Vepsalainen <bebraw@gmail.com>
2013-02-18 14:57:05 +0200

    Make rain effect look alright

--- public/examples/animation/rain.js
+++ public/examples/animation/rain.js
@@ -1,12 +1,13 @@
 function effect(cube) {
-    var water = ((ticks >> 7) % 7) + 1;
-    var surface = ticks & 127;
+    var limit = 100;
+    var water = ((ticks >> 10) % 7) + 1;
+    var surface = ticks % (limit + 1);
 
+    cube().off();
     cube({z: 0}).map(function(led) {
-        led.z = ((ticks >> 3) + math.random()) & 7;
+        led.z = cube.z - ((ticks >> 5) + math.random()) & 7;
 
         return led;
-    }).on(0.25);
-    cube({z: range(water, cube.z)}).on();
-    cube({z: range(water, cube.z)}).on(wf(surface << 1));
+    }).on(0.5);
+    cube({x: range(cube.x), z: range(water)}).on(0.8);
 }
