335f8ebd8f786896b79522438488c1e28d7eff6b
Juho Vepsalainen <bebraw@gmail.com>
2013-02-18 14:41:14 +0200

    Fix ilkka effect

--- public/examples/math/ilkka.js
+++ public/examples/math/ilkka.js
@@ -1,13 +1,15 @@
 function effect(cube) {
     var time = ticks / 100;
 
-    cube({z: 0}).each(function(led) {
+    cube().off();
+    cube({z: 0}).map(function(led) {
         var tc = math.cos(time);
         var ts = math.sin(time);
-        var x = led.x * tc - y * ts;
-        var y = led.x * ts + y * tc;
+        var x = (led.x * tc - led.y * ts);
+        var y = (led.x * ts + led.y * tc);
 
         led.z = (1 + math.sin(0.8 * (x + y + time)));
-        cube(led).on();
-    });
+
+        return led;
+    }).on();
 }
