2b130bbe82f981507569948c4b5131c8e6f3f20d
Juho Vepsalainen <bebraw@gmail.com>
2013-02-24 20:24:00 +0200

    Move auth to a module of its own

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var path = require('path');
 
 var express = require('express');
 var passport = require('passport');
-var gh = require('passport-github').Strategy;
 
 var routes = require('./routes');
+var auth = require('./auth');
 
 try {
     var conf = require('./conf.json');
@@ -51,44 +51,6 @@ function serve() {
     auth(app);
 
     return app;
-}
-
-// https://github.com/jaredhanson/passport-github/tree/master/examples/login
-function auth(app) {
-    passport.use(new gh({
-            clientID: conf.github.id,
-            clientSecret: conf.github.secret,
-            callbackURL: conf.host + ':' + conf.port + conf.github.url + '/callback'
-        },
-        function(accessToken, refreshToken, profile, done) {
-            process.nextTick(function () {
-                return done(null, profile);
-            });
-        }
-    ));
-
-    passport.serializeUser(function(user, done) {
-        done(null, user);
-    });
-
-    passport.deserializeUser(function(obj, done) {
-        done(null, obj);
-    });
-
-    app.get(conf.github.url, passport.authenticate('github'));
-
-    app.get(conf.github.url + '/callback', passport.authenticate('github', {
-            failureRedirect: '/'
-        }),
-        function(req, res) {
-            res.redirect('back');
-        }
-    );
-
-    app.get('/logout', function(req, res){
-        req.logout();
-        res.redirect('back');
-    });
 }
 
 module.exports = serve;
---
+++ auth.js
@@ -0,0 +1,44 @@
+var passport = require('passport');
+var gh = require('passport-github').Strategy;
+
+var conf = require('./conf.json');
+
+// https://github.com/jaredhanson/passport-github/tree/master/examples/login
+function auth(app) {
+    passport.use(new gh({
+            clientID: conf.github.id,
+            clientSecret: conf.github.secret,
+            callbackURL: conf.host + ':' + conf.port + conf.github.url + '/callback'
+        },
+        function(accessToken, refreshToken, profile, done) {
+            process.nextTick(function () {
+                return done(null, profile);
+            });
+        }
+    ));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function(obj, done) {
+        done(null, obj);
+    });
+
+    app.get(conf.github.url, passport.authenticate('github'));
+
+    app.get(conf.github.url + '/callback', passport.authenticate('github', {
+            failureRedirect: '/'
+        }),
+        function(req, res) {
+            res.redirect('back');
+        }
+    );
+
+    app.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('back');
+    });
+}
+
+module.exports = auth;
