3c291ceaeee1ba268c2c46403cee47bcd33c149e
Juho Vepsalainen <bebraw@gmail.com>
2013-02-24 20:38:33 +0200

    Move effect related aspects to a module of their own

---
+++ effects.js
@@ -0,0 +1,74 @@
+var fs = require('fs');
+var path = require('path');
+
+var gift = require('gift');
+
+var filewalker = require('filewalker');
+
+var conf = require('./conf.json');
+var repo = gift(conf.repoPath);
+
+function get(cb) {
+    var metaPath = effectsMetaPath();
+    var data = [];
+    var d;
+
+    filewalker(metaPath).
+        on('file', function(f) {
+            d = require(path.join(metaPath, f));
+            d.id = path.basename(f, '.json');
+
+            data.push(d);
+        }).
+        on('done', function() {
+            cb(data);
+        }).walk();
+}
+exports.get = get;
+
+function read(id, cb) {
+    fs.readFile(effectPath(id), 'utf8', cb);
+}
+exports.read = read;
+
+function commit(msg, id, data, cb) {
+    var p = effectPath(id);
+
+    fs.writeFile(p, data, 'utf8', function(err, d) {
+        if(err) return cb(err);
+
+        repo.add(path.join('effects', id + '.js'), function(err) {
+            if(err) {
+                console.log('add failed', err);
+                return cb(err);
+            }
+
+            repo.commit(msg, {
+                all: true
+            }, function(err) {
+                if(err) {
+                    console.log('commit failed', err);
+                    return cb(err);
+                }
+
+                cb();
+            });
+        });
+    });
+}
+exports.commit = commit;
+
+function effectPath(id) {
+    // avoid exposing FS (perhaps there's a neater way?)
+    if(id.indexOf('/') >= 0 || id.indexOf('\\') >= 0) return;
+
+    return path.join(effectsPath(), id + '.js');
+}
+
+function effectsPath() {
+    return path.join(conf.repoPath, 'effects');
+}
+
+function effectsMetaPath() {
+    return path.join(conf.repoPath, 'effects_meta');
+}
--- routes.js
+++ routes.js
@@ -1,39 +1,20 @@
-var fs = require('fs');
-var path = require('path');
-
-var filewalker = require('filewalker');
-var funkit = require('funkit');
-var gift = require('gift');
-
-var conf = require('./conf.json');
-var repo = gift(conf.repoPath);
+var effects = require('./effects');
 
 exports.index = function(req, res) {
-    var metaPath = effectsMetaPath();
-    var data = [];
-    var d;
-
-    filewalker(metaPath).
-        on('file', function(f) {
-            d = require(path.join(metaPath, f));
-            d.id = path.basename(f, '.json');
-
-            data.push(d);
-        }).
-        on('done', function() {
-            res.render('index', {
-                title: 'Elovalo Webide', // TODO: move to tpl
-                effects: data,
-                user: req.user
-            });
-        }).walk();
+    effects.get(function(data) {
+        res.render('index', {
+            title: 'Elovalo Webide', // TODO: move to tpl
+            effects: data,
+            user: req.user
+        });
+    });
 };
 
 exports.editor = function(req, res) {
     var id = req.query.id;
 
     if(id) {
-        readEffect(id, function(err, d) {
+        effects.read(id, function(err, d) {
             res.render('editor', {
                 title: 'Effect editor', // TODO: move to tpl
                 initialCode: d,
@@ -58,7 +39,7 @@ exports.editorSave = function(req, res) {
     var status;
 
     if(code && id) {
-        commit('Save effect', id, code, function(err) {
+        effects.commit('Save effect', id, code, function(err) {
             status = err? 'error': 'success';
 
             res.json({status: status});
@@ -73,7 +54,7 @@ exports.effects = function(req, res) {
     var id = req.query.id;
 
     if(id) {
-        readEffect(id, function(err, d) {
+        effects.read(id, function(err, d) {
             if(err) return res.send(404);
             else return res.send(d);
         });
@@ -82,48 +63,3 @@ exports.effects = function(req, res) {
         res.send(404);
     }
 };
-
-function readEffect(id, cb) {
-    fs.readFile(effectPath(id), 'utf8', cb);
-}
-
-function commit(msg, id, data, cb) {
-    var p = effectPath(id);
-
-    fs.writeFile(p, data, 'utf8', function(err, d) {
-        if(err) return cb(err);
-
-        repo.add(path.join('effects', id + '.js'), function(err) {
-            if(err) {
-                console.log('add failed', err);
-                return cb(err);
-            }
-
-            repo.commit(msg, {
-                all: true
-            }, function(err) {
-                if(err) {
-                    console.log('commit failed', err);
-                    return cb(err);
-                }
-
-                cb();
-            });
-        });
-    });
-}
-
-function effectPath(id) {
-    // avoid exposing FS (perhaps there's a neater way?)
-    if(id.indexOf('/') >= 0 || id.indexOf('\\') >= 0) return;
-
-    return path.join(effectsPath(), id + '.js');
-}
-
-function effectsPath() {
-    return path.join(conf.repoPath, 'effects');
-}
-
-function effectsMetaPath() {
-    return path.join(conf.repoPath, 'effects_meta');
-}
