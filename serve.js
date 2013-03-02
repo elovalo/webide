57b02f2cc1d6a7a0f7c0bef8f8292013ef8c0301
Juho Vepsalainen <bebraw@gmail.com>
2013-03-02 22:44:54 +0200

    Add serve script for server deployment

---
+++ serve.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+var app = require('./app')();
+var http = require('http');
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Starting Elovalo Webide at port " + app.get('port'));
+});
