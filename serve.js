b653e74c83b37566bdc972c8ea155438e13b64ae
Juho Vepsalainen <bebraw@gmail.com>
2013-03-18 21:09:44 +0200

    Fix formatting

--- serve.js
+++ serve.js
@@ -2,6 +2,6 @@
 var app = require('./app')();
 var http = require('http');
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
     console.log("Starting Elovalo Webide at port " + app.get('port'));
 });
